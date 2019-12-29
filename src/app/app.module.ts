dca047883759a17b230de08926cf6548de9eb530
titombo <titombo@gmail.com>
2019-12-29 11:59:43 +0100

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MatTabsModule } from '@angular/material';
+import {EditModeComponent} from './../edit-mode/edit-mode.component.ts'
+import {ViewModeComponent} from './../view-mode/view-mode.component.ts'
+
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule, MatTabsModule],
-  declarations: [AppComponent, HelloComponent],
+  imports: [BrowserModule,BrowserAnimationsModule, FormsModule, HttpClientModule, MatTabsModule],
+  declarations: [AppComponent, HelloComponent, EditModeComponent, ViewModeComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
